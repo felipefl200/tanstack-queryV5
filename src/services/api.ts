77171933b53692d1073636519e71fae2712f9ad7
unknown <felipfl200@gmail.com>
2024-09-17 17:13:29 -0300

    Add Mutations

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,4 +8,17 @@ export const api = ky.create({
   timeout: 10000,
 })
 
-export const getTodos = async () => await api.get<Todo[]>('todos').json()
+export const getTodos = async () =>
+  await api.get<Todo[]>('todos?_limit=08&_sort=id&_order=desc').json()
+
+export const getTodo = async (id: number) =>
+  (await api.get(`todos/${id}`).json()) as Todo
+
+export const createTodo = async (todo: Todo) =>
+  await api.post<Todo>('todos', { json: todo }).json()
+
+export const updateTodo = async (todo: Todo) =>
+  await api.put<Todo>(`todos/${todo.id}`, { json: todo }).json()
+
+export const deleteTodo = async (id: number) =>
+  await api.delete(`todos/${id}`).json()
