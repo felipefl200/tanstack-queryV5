25d139cfe3719b36cc631a44c99eecbb08eac65f
unknown <felipfl200@gmail.com>
2024-09-17 17:14:15 -0300

    Add Layout and Scaffold functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { MakeWith } from './components/make-with'
-import { useEffect, useState } from 'react'
-import ky from 'ky'
 import {
   Table,
   TableBody,
@@ -11,22 +9,104 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './components/ui/form'
+import { Checkbox } from './components/ui/checkbox'
+import { useGetTodos } from '@/services/queries'
+import { useForm } from 'react-hook-form'
+import { useCreateTodo } from './services/mutations'
+import { Todo } from './types/todo'
 
 function App() {
-  const [data, setData] = useState<Todo[]>([])
-  useEffect(() => {
-    ky.get('http://localhost:8000/todos')
-      .json()
-      .then((res) => {
-        setData(res as Todo[])
-      })
-  }, [])
+  const { data, isLoading } = useGetTodos()
+  const createTodo = useCreateTodo()
+
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+      checked: false,
+    },
+  })
+
+  const onSubmit = (data: Pick<Todo, 'title' | 'description' | 'checked'>) => {
+    createTodo.mutate(data)
+    form.reset()
+  }
+
   return (
     <div className="bg-slate-950 w-full h-screen flex flex-col justify-center items-center">
       <MakeWith className="fixed top-2 right-2" />
-      <div>
-        <Table className="text-slate-300 max-w-lg overflow-y-scroll">
-          <TableCaption>Lista recente de tarefas</TableCaption>
+      <div className="mt-2">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full p-4 mx-auto space-y-6 border shadow-md shadow-slate-900 border-slate-700 rounded-md"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-slate-400">
+                    Nome da tarefa
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} className="text-slate-400" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-slate-400">
+                    Descrição da tarefa
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} className="text-slate-400" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="checked"
+              render={({ field }) => (
+                <FormItem className="flex items-end gap-2">
+                  <FormControl>
+                    <Checkbox
+                      className="bg-slate-600 ring-slate-300 ring-1"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <FormLabel className="text-slate-400">Concluído</FormLabel>
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={createTodo.isPending}>
+              {createTodo.isPending ? 'Cadastrando...' : 'Cadastrar'}
+            </Button>
+          </form>
+        </Form>
+
+        <Table className="text-slate-300 max-w-lg w-[450px]">
+          <TableCaption>
+            {isLoading ? 'Carregando...' : 'Lista recente de tarefas'}
+          </TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">ID</TableHead>
@@ -35,21 +115,24 @@ function App() {
               <TableHead>Concluido</TableHead>
             </TableRow>
           </TableHeader>
-          <TableBody>
-            {data.map((todo) => (
-              <TableRow key={todo.id}>
-                <TableCell className="font-medium">{todo.title}</TableCell>
-                <TableCell>{todo.title}</TableCell>
-                <TableCell>{todo.description}</TableCell>
-                <TableCell>{todo.checked ? 'Sim' : 'Não'}</TableCell>
-              </TableRow>
-            ))}
+
+          <TableBody className="">
+            {data &&
+              data.map((todo) => (
+                <TableRow key={todo.id}>
+                  <TableCell className="font-medium">{todo.id}</TableCell>
+                  <TableCell>{todo.title}</TableCell>
+                  <TableCell>{todo.description}</TableCell>
+                  <TableCell>{todo.checked ? 'Sim' : 'Não'}</TableCell>
+                </TableRow>
+              ))}
           </TableBody>
+
           <TableFooter>
             <TableRow>
               <TableCell colSpan={3}>Total concluídos:</TableCell>
               <TableCell className="text-right">
-                {data.filter((todo) => todo.checked).length}
+                {data && data.filter((todo) => todo.checked).length}
               </TableCell>
             </TableRow>
           </TableFooter>
